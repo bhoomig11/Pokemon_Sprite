f8edaa5f446905c67d45bbd00c69d5ee12045286
Bhoomika <bhoomig252@gmail.com>
2025-05-30 14:47:56 -0400

    fentire project

---
+++ index.js
@@ -0,0 +1,41 @@
+// fetch - Function used for making HTTP requests to fetch resources.
+//         (JSON style data, images, files)
+//         Simplifies asynchronous data fetching in JavaScript and
+//         used for interacting with APIs to retrieve and send
+//         data aschronously over the web.
+//         fetch(url, {options}) - default is GET
+
+// fetch resource using promise:
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachus")
+//      .then(response => {
+//         if(!response.ok) {
+//             throw new Error("Could not fetch resource")
+//         }
+//         return response.json();
+//      })
+//      .then(data => console.log(data))
+//      .catch(error => console.error(error));
+
+// fetch resource using async/await
+
+async function fetchData(){
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonImage = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonImg");
+
+        imgElement.src = pokemonImage;
+        imgElement.style.display = "block";
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
